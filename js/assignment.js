35283f3d632294b2b7e275e1a4114410379615ab
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-08-28 21:23:42 +0600

    heart icon functionality added

--- js/assignment.js
+++ js/assignment.js
@@ -1,1 +1,11 @@
-console.log("file connected");
+/* heart icon related functionality */
+
+const allHeartValue = document.getElementsByClassName('heart-icon');
+for(const singleValue of allHeartValue){
+    singleValue.addEventListener('click', function(){
+        let count = parseInt(document.getElementById('initial-heart-value').innerText);
+        count++;
+        document.getElementById('initial-heart-value').innerText= count;
+    })
+}
+
